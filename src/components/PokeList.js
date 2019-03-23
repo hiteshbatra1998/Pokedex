78544d0ae70bea1bd236607b063b1bc108b11dd7
Hitesh Batra <32892304+hiteshbatra1998@users.noreply.github.com>
2019-03-23 14:51:49 +0530

    Update PokeList.js

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,24 +1,23 @@
 import React from 'react';
-
-// const PokeList = () =>{
-//     render(){
-//         return(
-//             <div className="Poke-Container">
-//                 Hi
-//             </div>
-//         );
-//     }
-// };
+import PokeCss from '../Styling/Poke.css';
 
 const PokeList = (props)=>{
+    let i=0;
     const names=props.list.map(
         (poke)=>{
+            i=i+1;
+            const url=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
             if( poke.name.includes(props.str) )
-                return <div key={poke.name}>{poke.name}</div>
+                return (
+                        <div key={poke.name} className="contain">
+                            <div className="image-div"><img className="image" src={url} alt="poke-img"/></div>
+                            <div className="name">{poke.name}</div>
+                        </div>
+                        );
         }
     );
     return(
-        <div>
+        <div className="PokeContain">
             {names}
             
         </div>
